a3aae59e71c12df39aa334479971fad8ef9aa0d4
thanhnhan <nhanthanhdovu@gmail.com>
2021-06-25 00:05:55 +0700

    fix

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -15,9 +15,7 @@ import {
     PASSWORD_VALIDATOR,
     EMAIL_VALIDATOR,
 } from "../../constants/FormValidator";
-const reEmail =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//
+
 export default function Register() {
     const dispatch = useDispatch();
     const history = useHistory();
