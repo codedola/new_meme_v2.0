c40442930c25c91290dc67d3fd5d91e84b8a2f99
thanhnhan <nhanthanhdovu@gmail.com>
2021-08-19 22:27:13 +0700

    fix thunkItems

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,11 @@ import logger from "redux-logger";
 import thunkMiddle from "redux-thunk";
 
 let thunkItems = [];
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
     thunkItems.push(logger);
+    if (window.location.href.includes("herokuapp")) {
+        thunkItems = [];
+    }
 } else {
     thunkItems = [];
 }
